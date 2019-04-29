f45afb488a6a0750bbe83404fed4c36c8b9a2103
Devendra K. Rane <ranedk@gmail.com>
2019-04-29 21:12:05 +0530

    db settings update

--- app/imdb/imdb/imdb/settings.py
+++ app/imdb/imdb/imdb/settings.py
@@ -74,11 +74,14 @@ WSGI_APPLICATION = 'imdb.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'imdb',
+        'USER': 'imdb',
+        'PASSWORD': 'imdb',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
