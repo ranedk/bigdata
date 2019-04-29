30d58739a4d7cc9bf83850a1b9406a894a90dff8
Devendra K. Rane <ranedk@gmail.com>
2019-04-29 17:01:58 +0530

    syntax

--- app/imdb/imdb/core/models.py
+++ app/imdb/imdb/core/models.py
@@ -27,7 +27,7 @@ class Category(models.Model):
 
 class Title(models.Model):
     title_id = models.CharField(max_length=50)
-    title_format = models.ForeignKey('Format')
+    title_format = models.ForeignKey('Format', on_delete=models.CASCADE)
     primary_title = models.CharField(max_length=255)
     original_title = models.CharField(max_length=255)
     is_adult = models.BooleanField()
@@ -38,11 +38,11 @@ class Title(models.Model):
 
 
 class TitleAka(models.Model):
-    title = models.ForeignKey('Title')
+    title = models.ForeignKey('Title', on_delete=models.CASCADE)
     ordering = models.IntegerField()
     name = models.CharField(max_length=255)
-    region = models.ForeignKey('Region')
-    language = models.ForeignKey('Language')
+    region = models.ForeignKey('Region', on_delete=models.CASCADE)
+    language = models.ForeignKey('Language', on_delete=models.CASCADE)
     ttypes = models.CharField(max_length=10)
     attributes = models.CharField(max_length=255)
     is_original_title = models.BooleanField()
@@ -58,30 +58,28 @@ class Person(models.Model):
 
 
 class Crew(models.Model):
-    title = models.ForeignKey('Title')
+    title = models.ForeignKey('Title', on_delete=models.CASCADE)
     directors = models.ManyToManyField('Person')
     writers = models.ManyToManyField('Writers')
 
 
 class Principal(models.Model):
-    title = models.ForeignKey('Title')
+    title = models.ForeignKey('Title', on_delete=models.CASCADE)
     ordering = models.IntegerField()
-    person = models.ForeignKey('Person')
-    category = models.ForeignKey('Category')
+    person = models.ForeignKey('Person', on_delete=models.CASCADE)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
     job = models.CharField(max_length=255)
     characters = models.CharField(max_length=255)
 
 
 class Rating(models.Model):
-    title = models.ForeignKey('Title')
+    title = models.ForeignKey('Title', on_delete=models.CASCADE)
     avg_rating = models.IntegerField()
     number_of_votes = models.IntegerField()
 
 
 class Episode(models.Model):
-    title = models.ForeignKey('Title')
-    parent_title = models.ForeignKey('Title')
+    title = models.ForeignKey('Title', on_delete=models.CASCADE)
+    parent_title = models.ForeignKey('Title', on_delete=models.CASCADE)
     season_number = models.IntegerField()
     episode_number = models.IntegerField()
-
-
